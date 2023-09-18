f44eef9c1e7b787b203ae207f49a55e7241489ee
Lucy Pennington <lpenning@ed.ac.uk>
2023-09-18 13:56:15 +0100

    api config update

--- frontend/src/api/ApiCall.js
+++ frontend/src/api/ApiCall.js
@@ -1,6 +1,6 @@
 export async function fetchData(query) {
   try {
-    const response = await fetch("http://localhost:5001/api/" +query);
+    const response = await fetch("http://localhost:5000/api/" +query);
     const data = await response.json();
     return data;
   } catch (error) {
