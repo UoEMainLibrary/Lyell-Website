16d5a2f05464996dff0b6fc4c3b2de607ca57cd9
Lucy Pennington <lpenning@ed.ac.uk>
2023-09-18 14:52:26 +0100

    api fix

--- frontend/src/api/ApiCall.js
+++ frontend/src/api/ApiCall.js
@@ -1,6 +1,6 @@
 export async function fetchData(query) {
   try {
-    const response = await fetch("http://127.0.0.1:5000/api/" +query);
+    const response = await fetch("http://lac-lyell-test.is.ed.ac.uk:5000/api/" +query);
     const data = await response.json();
     return data;
   } catch (error) {
