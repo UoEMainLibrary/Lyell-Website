cb537c2ef0c5d1bd0aa0e164996bb4c1fdd915d1
Lucy Pennington <lpenning@ed.ac.uk>
2024-01-16 11:43:05 +0000

    name fixes

--- frontend/src/api/ApiCall.js
+++ frontend/src/api/ApiCall.js
@@ -1,6 +1,7 @@
 export async function fetchData(query) {
   try {
     const response = await fetch("http://localhost:5000/api/" +query);
+    // const response = await fetch("https://test.lyell.ed.ac.uk/api/" +query);
     const data = await response.json();
     return data;
   } catch (error) {
