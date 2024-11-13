a231fe5dbbe9edd370aeaea2dd198746896f3efc
Lucy <lucygpen@hotmail.com>
2024-11-13 16:19:31 +0000

    accessibility status

--- frontend/src/pages/Accessibility.js
+++ frontend/src/pages/Accessibility.js
@@ -99,13 +99,14 @@ const AccessibilityStatement = ({
                 a few simple steps you can customise the appearance of our website
                 using your browser settings to make it easier to read and navigate:</p>
             <p><a href="https://www.ed.ac.uk/about/website/accessibility/customising-site"
-                  target="Customising our site"  style={styles.link}>Additional
+                  target="Customising our site" style={styles.link}>Additional
                 information on how to customise our website appearance</a></p>
             <p>If
                 you are a member of University staff or a student, you can use the free SensusAccess accessible document
                 conversion service:</p>
             <p><a
-                href="https://www.ed.ac.uk/student-disability-service/staff/supporting-students/accessible-technology"  style={styles.link}>Information
+                href="https://www.ed.ac.uk/student-disability-service/staff/supporting-students/accessible-technology"
+                style={styles.link}>Information
                 on SensusAccess</a></p>
             <h2 style={styles.h2}>How accessible this website is</h2>
             <p>We know some parts of this website are not fully accessible:</p>
@@ -121,23 +122,24 @@ const AccessibilityStatement = ({
                 contact us:
             </p>
             <ul>
-                <li>Or phoning:</li>
-                <ul>
-                    <li>+44 (0)131 651 5151</li>
-                </ul>
+                <li>By using the IS Helpline online contact form:</li>
             </ul>
+            <p><a href="https://www.ishelpline.ed.ac.uk/forms/">IS Helpline contact form</a></p>
+            <ul>
+                <li>Or phoning:</li>
+            </ul>
+            <p>+44 (0)131 651 5151</p>
 
             <ul>
                 <li>Or email:</li>
-                <ul>
-                    <li><a href="mailto:Information.systems@ed.ac.uk"  style={styles.link}>Information.systems@ed.ac.uk</a></li>
-                </ul>
             </ul>
+            <p><a href="mailto:Information.systems@ed.ac.uk"
+                  style={styles.link}>Information.systems@ed.ac.uk</a></p>
 
             <p>British Sign Language (BSL) users can contact us via&nbsp;Contact Scotland
                 BSL, the on-line BSL interpreting service:</p>
             <p>
-                <a href="https://contactscotland-bsl.org/"  style={styles.link}>Contact
+                <a href="https://contactscotland-bsl.org/" style={styles.link}>Contact
                     Scotland BSL</a>
             </p>
             <p>We’ll consider your request and get back to you in 5 working days.</p>
@@ -148,25 +150,26 @@ const AccessibilityStatement = ({
                 meeting accessibility requirements, please contact us:&nbsp;
             </p>
             <ul>
-                <li>Or phoning:</li>
-                <ul>
-                    <li>+44 (0)131 651 5151</li>
-                </ul>
+                <li>By using the IS Helpline online contact form:</li>
             </ul>
+            <p><a href="https://www.ishelpline.ed.ac.uk/forms/">IS Helpline contact form</a></p>
+            <ul>
+                <li>Or phoning:</li>
+            </ul>
+            <p>+44 (0)131 651 5151</p>
 
             <ul>
                 <li>Or email:</li>
-                <ul>
-                    <li><a href="mailto:Information.systems@ed.ac.uk"  style={styles.link}>Information.systems@ed.ac.uk</a></li>
-                </ul>
             </ul>
+            <p><a href="mailto:Information.systems@ed.ac.uk"
+                  style={styles.link}>Information.systems@ed.ac.uk</a></p>
             <p>
                 British Sign Language (BSL) users can
                 contact us via&nbsp;Contact Scotland BSL, the on-line BSL
                 interpreting service:
             </p>
             <p>
-                <a href="https://contactscotland-bsl.org/"  style={styles.link}>Contact
+                <a href="https://contactscotland-bsl.org/" style={styles.link}>Contact
                     Scotland BSL</a>
             </p>
             <p>We
@@ -181,13 +184,14 @@ const AccessibilityStatement = ({
                 regulations’). If you’re not happy with how we respond to your
                 complaint please contact the Equality Advisory and Support Service
                 (EASS) directly:</p>
-            <p><a href="https://www.equalityadvisoryservice.com/"  style={styles.link}>Contact
+            <p><a href="https://www.equalityadvisoryservice.com/" style={styles.link}>Contact
                 details for the Equality Advisory and Support Service (EASS)</a></p>
 
             <p>The
                 government has produced information on how to report accessibility
                 issues:</p>
-            <p><a href="https://www.gov.uk/reporting-accessibility-problem-public-sector-website" style={styles.link}>Reporting
+            <p><a href="https://www.gov.uk/reporting-accessibility-problem-public-sector-website"
+                  style={styles.link}>Reporting
                 an accessibility problem on a public sector website</a></p>
 
             <h2 style={styles.h2}>Contacting us by phone using British Sign Language Service</h2>
@@ -199,7 +203,7 @@ const AccessibilityStatement = ({
                 of Scotland’s public bodies using video relay. This enables sign
                 language users to contact public bodies and vice versa. The service
                 operates from 8.00am to 12.00am, 7 days a week.</p>
-            <p><a href="https://contactscotland-bsl.org/"  style={styles.link}>Contact
+            <p><a href="https://contactscotland-bsl.org/" style={styles.link}>Contact
                 Scotland BSL service details.</a></p>
 
             <h2 style={styles.h2}>Technical information about this website’s accessibility</h2>
@@ -209,12 +213,13 @@ const AccessibilityStatement = ({
                 (Websites and Mobile Applications) (No. 2) Accessibility Regulations
                 2018.</p>
             <h2 style={styles.h2}>Compliance Status</h2>
-            <p>This website is partially compliant with the Web Content Accessibility Guidelines (WCAG) 2.2 AA standard,
+            <p>This website is partially compliant with the Web Content Accessibility Guidelines (WCAG) 2.2 AA
+                standard,
                 due to
                 the non-compliances listed below.</p>
             <p>The
                 full guidelines are available at:</p>
-            <p><a href="https://www.w3.org/TR/WCAG22/"  style={styles.link}>Web
+            <p><a href="https://www.w3.org/TR/WCAG22/" style={styles.link}>Web
                 Content Accessibility Guidelines (WCAG) 2.2 AA standard</a></p>
 
             <h2 style={styles.h2}>Non accessible content</h2>
@@ -229,92 +234,91 @@ const AccessibilityStatement = ({
 
             <ul>
                 <li>Not all non-text items presented to users has alternative text</li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#non-text-content"  style={styles.link}>1.1.1 - Non Text Content</a></li>
-                </ul>
+                <li><a href="https://www.w3.org/TR/WCAG22/#non-text-content" style={styles.link}>1.1.1 - Non Text
+                    Content</a></li>
             </ul>
 
             <ul>
                 <li>The site has functionality issues in portrait mode on Android devices</li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#orientation"  style={styles.link}>1.3.4 - Orientation</a></li>
-                </ul>
+                <li><a href="https://www.w3.org/TR/WCAG22/#orientation" style={styles.link}>1.3.4 - Orientation</a>
+                </li>
             </ul>
 
             <ul>
                 <li>Some hyperlinks are conveyed with colour only</li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#use-of-color"  style={styles.link}>1.4.1 - Use of Colour</a></li>
-                </ul>
+                <li><a href="https://www.w3.org/TR/WCAG22/#use-of-color" style={styles.link}>1.4.1 - Use of
+                    Colour</a>
+                </li>
+
             </ul>
             <ul>
                 <li>Some text may not have sufficient colour contrast between font and background colours</li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#contrast-minimum"  style={styles.link}>1.4.3 - Contrast (Minimum)</a></li>
-                </ul>
+                <li><a href="https://www.w3.org/TR/WCAG22/#contrast-minimum" style={styles.link}>1.4.3 - Contrast
+                    (Minimum)</a></li>
+
             </ul>
 
             <ul>
-                <li>Some items are examples of text as an image and do not contain the appropriate alternative text</li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#images-of-text"  style={styles.link}>1.4.5 - Images of Text</a></li>
-                </ul>
+                <li>Some items are examples of text as an image and do not contain the appropriate alternative
+                    text
+                </li>
+                <li><a href="https://www.w3.org/TR/WCAG22/#images-of-text" style={styles.link}>1.4.5 - Images of
+                    Text</a></li>
             </ul>
 
             <ul>
                 <li>Reflow is not operational up to 400%</li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#reflow"  style={styles.link}>1.4.10 - Reflow</a></li>
-                </ul>
+                <li><a href="https://www.w3.org/TR/WCAG22/#reflow" style={styles.link}>1.4.10 - Reflow</a></li>
             </ul>
             <ul>
                 <li>No skip to main content button is enabled on some pages of the site</li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#bypass-blocks"  style={styles.link}>2.4.1 - Bypass Blocks</a></li>
-                </ul>
+                <li><a href="https://www.w3.org/TR/WCAG22/#bypass-blocks" style={styles.link}>2.4.1 - Bypass
+                    Blocks</a>
+                </li>
             </ul>
             <ul>
                 <li>Some links do not contain meaningful hypertext to inform the user of their target location</li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#link-purpose-in-context"  style={styles.link}>2.4.4 - Link Purpose (In
-                        Context)</a>
-                    </li>
-                </ul>
+                <li><a href="https://www.w3.org/TR/WCAG22/#link-purpose-in-context" style={styles.link}>2.4.4 - Link
+                    Purpose (In
+                    Context)</a>
+                </li>
             </ul>
             <ul>
-                <li>Headings are not formatted correctly and some heading levels are missed, which affects screen reader
+                <li>Headings are not formatted correctly and some heading levels are missed, which affects screen
+                    reader
                     software
                 </li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#headings-and-labels"  style={styles.link}>2.4.6 - Headings and Labels</a></li>
-                </ul>
+                <li><a href="https://www.w3.org/TR/WCAG22/#headings-and-labels" style={styles.link}>2.4.6 - Headings
+                    and
+                    Labels</a></li>
             </ul>
             <ul>
                 <li>It is not always clear where you have tabbed to using a keyboard</li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#focus-visible"  style={styles.link}>2.4.7 - Focus Visible</a></li>
-                </ul>
+                <li><a href="https://www.w3.org/TR/WCAG22/#focus-visible" style={styles.link}>2.4.7 - Focus
+                    Visible</a>
+                </li>
             </ul>
             <ul>
                 <li>Some hyperlinks open link in new tab / window, and some pop-ups do not alert the user this will
                     happen
                 </li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#on-input"  style={styles.link}>3.2.2 - On Input</a></li>
-                </ul>
+                <li><a href="https://www.w3.org/TR/WCAG22/#on-input" style={styles.link}>3.2.2 - On Input</a></li>
             </ul>
             <ul>
-                <li>Help such as contact us and the chat function are not located in the same place on every page.</li>
-                <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#consistent-help" style={styles.link}>3.2.6 - Consistent Help</a></li>
-                </ul>
+                <li>Help such as contact us and the chat function are not located in the same place on every page.
+                </li>
+                <li><a href="https://www.w3.org/TR/WCAG22/#consistent-help" style={styles.link}>3.2.6 - Consistent
+                    Help</a></li>
             </ul>
             <ul>
-                <li>The website is not fully compatible with assistive software e.g. – using assistive software it is
+                <li>The website is not fully compatible with assistive software e.g. – using assistive software it
+                    is
                     not possible to reach all content
                 </li>
                 <ul>
-                    <li><a href="https://www.w3.org/TR/WCAG22/#name-role-value" style={styles.link}>4.1.2 - Name, Role, Value</a></li>
+                    <li><a href="https://www.w3.org/TR/WCAG22/#name-role-value" style={styles.link}>4.1.2 - Name,
+                        Role,
+                        Value</a></li>
                 </ul>
             </ul>
             <p>We
@@ -334,13 +338,15 @@ const AccessibilityStatement = ({
             <p>At
                 this time we believe no content is outwith the scope of the accessibility regulations.</p>
             <h2 style={styles.h2}>What we’re doing to improve accessibility</h2>
-            <p>We will continue to address and make significant improvements to the accessibility issues highlighted.
+            <p>We will continue to address and make significant improvements to the accessibility issues
+                highlighted.
                 Unless specified otherwise, a complete solution or significant improvement will be in place
                 by {testDate}.
             </p>
             <p>
                 While we are in the process of resolving these accessibility issues we will ensure reasonable
-                adjustments are in place to make sure no user is disadvantaged. As changes are made, we will continue to
+                adjustments are in place to make sure no user is disadvantaged. As changes are made, we will
+                continue to
                 review accessibility and retest the accessibility of this website.
             </p>
             <h2 style={styles.h2}>Preparation of this accessibility statement</h2>
@@ -424,8 +430,10 @@ const AccessibilityStatement = ({
                 </li>
             </ul>
             <h2 style={styles.h2}>Change Log</h2>
-            <p>Since our initial report, we have undertaken more extensive manual testing with assistive software to get
-                a better understanding of the accessibility issues on this website. This section will receive updates as
+            <p>Since our initial report, we have undertaken more extensive manual testing with assistive software to
+                get
+                a better understanding of the accessibility issues on this website. This section will receive
+                updates as
                 and when accessibility improvements are made to the website.</p>
 
         </div>
